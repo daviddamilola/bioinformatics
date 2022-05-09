6c89f97e70be3a6339deb16d8db8ecdc46920515
Ekene Ashinze <ashinzekene@gmail.com>
2022-05-09 16:58:32 +0100

    ....

--- 3-comparing-genes-proteins-and-genomes/week_3/linear_space_alignment.py
+++ 3-comparing-genes-proteins-and-genomes/week_3/linear_space_alignment.py
@@ -12,26 +12,38 @@ def LinearSpaceAlignment(v, w, top, bottom, left, right, indel, stack):
     else:
         mid_node, next_node, direction = ModifiedMiddleEgde(v, w, top, bottom, left, right, indel)
         LinearSpaceAlignment(v, w, top, mid_node[0], left, mid_node[1], indel, stack)
+        print("{} and {}, direction {}, middle ".format(
+            v[top:bottom], w[left:right], direction), mid_node[0]+top, mid_node[1]+left)
         stack.append(direction)
         LinearSpaceAlignment(v, w, next_node[0], bottom, next_node[1], right, indel, stack)
 
 def OutputLCS(backtrack, v, w):
     v_result = "" 
     w_result = "" 
+    v_index = 0
+    w_index = 0
     for i, direction in enumerate(backtrack):
         if direction == "+":
-            v_result += v[i]
-            w_result += v[i]
+            v_result += v[v_index]
+            w_result += w[w_index]
+            v_index += 1
+            w_index += 1
         elif direction == "-":
-            v_result += v[i]
+            v_result += v[v_index]
             w_result += "-"
+            v_index += 1
         else:
             v_result += "-"
-            w_result += w[i]
-    
-w = "MEANLY"
-v = "PLEASANTLY"
+            w_result += w[w_index]
+            w_index += 1
+    return v_result, w_result
+
+
+v = "MEANLY"
+w = "PLEASANTLY"
 stack = []
 LinearSpaceAlignment(v, w, 0, len(v), 0, len(w), 5, stack)
-result = list("".join(filter(lambda x: x, stack)))
-print(OutputLCS(result, v, w))
+result = "".join(filter(lambda x: x, stack))
+result = result[::-1]
+print(result)
+print(OutputLCS(result, w, v))
